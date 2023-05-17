89a156a3e4520f80336daf810fd7f9c1385c3bd4
Samantha Capps <scapps00@gmail.com>
2023-05-17 19:21:59 -0400

    getting started with the react ui

---
+++ pages/components/Game.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Head from "next/head";
+import { Grid } from "@mui/material";
+import { loadComponents } from "next/dist/server/load-components";
+
+const Game = () => {};
